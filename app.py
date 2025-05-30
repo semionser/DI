244c555d1e257456573b883474df0a58fd3b5f9a
Майков Семен Андреевич <Maikov.SA@tpprf.loc>
2025-05-30 14:03:30 +0300

    first commit

--- app.py
+++ app.py
@@ -10,4 +10,4 @@ def valentine():
     return render_template('valentine.html', start_date=start_date.strftime("%Y-%m-%dT%H:%M:%S"))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
