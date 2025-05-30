d1422910a7ddbf29a7c5d175df65c2e810ec5301
Майков Семен Андреевич <Maikov.SA@tpprf.loc>
2025-05-30 16:05:38 +0300

    first commit

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-start_date = datetime(2023, 11, 1, 0, 0, 0)  # Укажи свою дату здесь
-
+start_date = datetime(2023, 12, 1, 0, 0, 0)
 @app.route('/')
 def valentine():
+
     return render_template('valentine.html', start_date=start_date.strftime("%Y-%m-%dT%H:%M:%S"))
 
 if __name__ == '__main__':
