e8280c825aafc3bb68f98f45c4db8b2adccda716
Майков Семен Андреевич <Maikov.SA@tpprf.loc>
2025-05-30 13:32:06 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+start_date = datetime(2000, 1, 12, 0, 0, 0)  # Укажи свою дату здесь
+
+@app.route('/')
+def valentine():
+    return render_template('valentine.html', start_date=start_date.strftime("%Y-%m-%dT%H:%M:%S"))
+
+if __name__ == '__main__':
+    app.run(debug=True)
